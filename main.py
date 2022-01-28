9f6a10320bfa81e97318eb964a6cf61fcd59f59e
Beto <beto3971@gmail.com>
2022-01-28 19:35:57 -0400

    updated main to convert report

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import jpype
 import xlsxwriter
-import io
+
 from xlsx2html import xlsx2html
-import re
+import pdfkit
+
+
+from bs4 import BeautifulSoup
 
 
 def print_excel():
@@ -30,38 +34,29 @@ def print_excel():
     # Write a total using a formula.
     worksheet.write(row, 0, 'Total')
     worksheet.write(row, 1, '=SUM(B1:B4)')
+    #workbook.save('Expenses01.pdf', SaveFormat.PDF)
 
     workbook.close()
 
 
+
 def trasnf_report_html():
-    # must be binary mode
-    # xlsx_file = open('/home/beto/Documents/Projects/Pruebra-reporte/Expenses01.xlsx', 'rb')
-    # out_file = io.StringIO()
-    # xlsx2html(xlsx_file, out_file, locale='en')
-    # out_file.seek(0)
-    # result_html = out_file.read()
-    # print(result_html)
     xlsx2html('./Expenses01.xlsx', './Expenses01.html')
+    with open('./Expenses01.html') as f:
+        pdfkit.from_file(f, './Expenses01.pdf')
 
 def extrac_table():
-    test_str = open('./Expenses01.html', 'rb')
-    tag = "b"
-    print(test_str)
-
-    reg_str = "<" + tag + ">(.*?)</" + tag + ">"
-    res = re.findall(reg_str, test_str)
-
-    print("The Strings extracted : " + res)
+    with open('./Expenses01.html') as response:
+        content = response.read()
+    bs = BeautifulSoup(content, 'html.parser')
+    tables = bs.find_all('table')
+    return tables[0]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     print_excel()
     trasnf_report_html()
-    extrac_table()
+    table = extrac_table()
 
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
